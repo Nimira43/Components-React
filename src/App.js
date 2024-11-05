962a4cda475d26537cd476b186b1325320e90c16
Nimira43 <n.rabone72@aol.com>
2024-11-05 00:50:24 +0000

    Rebooted repo. Will create now a components project to create various components from scratch.

---
+++ src/App.js
@@ -0,0 +1,7 @@
+function App() {
+  return (
+    <h1>Components React</h1>
+  )
+}
+
+export default App
---
+++ src/Button.js
@@ -0,0 +1,9 @@
+function Button() {
+  return(
+    <button>
+      Default
+    </button>
+  )
+}
+
+export default Button
