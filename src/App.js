343d8fc308926ffc88aad0126a86e12a6bc03c50
Nimira43 <n.rabone72@aol.com>
2024-11-16 11:48:05 +0000

    Placing the 16 button display demo into new components folder for reference and to clean up the main App.js file

--- src/App.js
+++ src/App.js
@@ -3,30 +3,20 @@ import './index.css'
 
 function App() {
   return (
-    <>
-      <h1 className='temp-heading'>Components React</h1>
-      <div className='solid'>
-        <Button />
-        <button className='btn-dark'>Dark</button>
-        <button className='btn-light'>Light</button>
-        <button className='btn-mist'>Mist</button>
-        <button className='btn-wisdom'>Wisdom</button>
-        <button className='btn-ember'>Ember</button>
-        <button className='btn-accept'>Accept</button>
-        <button className='btn-gold'>Gold</button>
-        <button className='btn-warm'>Warm</button>
+    <div>
+      <div>
+        <Button text='Submit'></Button>
       </div>
-      <div className='outline'>  
-        <button className='btn-dark-outline'>Dark</button>
-        <button className='btn-light-outline'>Light</button>
-        <button className='btn-mist-outline'>Mist</button>
-        <button className='btn-wisdom-outline'>Wisdom</button>
-        <button className='btn-ember-outline'>Ember</button>
-        <button className='btn-accept-outline'>Accept</button>
-        <button className='btn-gold-outline'>Gold</button>
-        <button className='btn-warm-outline'>Warm</button>
+      <div>
+        <Button text='Submit'></Button>
       </div>
-    </>
+      <div>
+        <Button text='Submit'></Button>
+      </div>
+      <div>
+        <Button text='Submit'></Button>
+      </div>
+    </div>
   )
 }
 
---
+++ src/components/ButtonDisplayTemplate.js
@@ -0,0 +1,33 @@
+import Button from '../Button'
+import '../index.css'
+
+function ButtonDisplayTemplate() {
+  return (
+    <>
+      <h1 className='temp-heading'>Components React</h1>
+      <div className='solid'>
+        <Button />
+        <button className='btn-dark'>Dark</button>
+        <button className='btn-light'>Light</button>
+        <button className='btn-mist'>Mist</button>
+        <button className='btn-wisdom'>Wisdom</button>
+        <button className='btn-ember'>Ember</button>
+        <button className='btn-accept'>Accept</button>
+        <button className='btn-gold'>Gold</button>
+        <button className='btn-warm'>Warm</button>
+      </div>
+      <div className='outline'>  
+        <button className='btn-dark-outline'>Dark</button>
+        <button className='btn-light-outline'>Light</button>
+        <button className='btn-mist-outline'>Mist</button>
+        <button className='btn-wisdom-outline'>Wisdom</button>
+        <button className='btn-ember-outline'>Ember</button>
+        <button className='btn-accept-outline'>Accept</button>
+        <button className='btn-gold-outline'>Gold</button>
+        <button className='btn-warm-outline'>Warm</button>
+      </div>
+    </>
+  )
+}
+
+export default ButtonDisplayTemplate
