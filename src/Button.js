4761cd9a6421f20d037fee22c06ababb8fb15a0d
Nimira43 <n.rabone72@aol.com>
2025-03-15 09:34:57 +0000

    Remove the light class of buttons - not needed.

--- src/Button.js
+++ src/Button.js
@@ -7,7 +7,6 @@ function Button({
   children,
   prime,
   support,
-  light,
   dark, 
   forest, 
   alert,
@@ -20,14 +19,13 @@ function Button({
     className('px-3 py-1.5 border rounded-md', {
       'border-prime bg-prime text-light': prime,
       'border-support bg-support text-light': support,
-      'border-light bg-light text-dark': light,
       'border-dark bg-dark text-light': dark,
       'border-forest bg-forest text-light': forest,
       'border-alert bg-alert text-dark': alert,
       'border-blaze bg-blaze text-light': blaze,
       'border-inform bg-inform text-light': inform,
       'rounded-full': rounded,
-      'bg-white': outline,
+      'bg-light': outline,
       'text-prime': outline && prime,
       'text-support': outline && support,
       'text-dark': outline && dark,
@@ -49,7 +47,6 @@ Button.propTypes = {
   prime: PropTypes.bool,
   support: PropTypes.bool,
   dark: PropTypes.bool,
-  light: PropTypes.bool,
   forest: PropTypes.bool,
   alert: PropTypes.bool,
   blaze: PropTypes.bool,
@@ -63,7 +60,6 @@ Button.propTypes.checkVariationValue = ({
   prime,
   support,
   dark,
-  light,
   forest,
   alert,
   blaze,
@@ -73,14 +69,13 @@ Button.propTypes.checkVariationValue = ({
     Number(!!prime) +
     Number(!!support) +
     Number(!!dark) +
-    Number(!!light) +
     Number(!!forest) +
     Number(!!alert) +
     Number(!!blaze) +
     Number(!!inform)
 
   if (count > 1) {
-    return new Error('Only one of prime, support, dark, light, forest, alert, blaze or inform can be true')
+    return new Error('Only one of prime, support, dark,forest, alert, blaze or inform can be true')
   }
 }
 
