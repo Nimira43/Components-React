6210141d29d0615759de3c2906250bad20c69b27
Nimira43 <n.rabone72@aol.com>
2025-04-03 03:53:33 +0100

    Archiving current status of project and labelling it Version 2. Next up will be a refactor of the present code.

---
+++ archive/version-2/src/App.js
@@ -0,0 +1,105 @@
+import React from 'react'
+import Button from './Button'
+import './index.css'
+import { MdOutlineLogin, MdOutlineLogout,MdPowerSettingsNew } from "react-icons/md"
+import { AiOutlineHome } from "react-icons/ai"
+import { LuSettings } from "react-icons/lu"
+import { TfiFaceSmile, TfiFaceSad } from "react-icons/tfi"
+import { RiUserLine } from "react-icons/ri"
+import { IoMdInformationCircleOutline } from "react-icons/io"
+import { IoLocationOutline } from "react-icons/io5"
+import { HiArrowLeft, HiArrowRight } from "react-icons/hi"
+
+function App() {
+  
+  return (
+    <div className='bg-[#777] p-10'>
+      <div>
+        <Button prime className='mb-5'>
+          <MdOutlineLogin />
+          Login
+        </Button>
+      </div>
+      <div>
+        <Button support>
+          <RiUserLine />
+          Register
+        </Button>
+      </div>
+      <div>
+        <Button dark>
+          <LuSettings />
+          Settings
+        </Button>
+      </div>
+      <div>
+        <Button light>
+          <IoLocationOutline />
+          City?
+        </Button>
+      </div>
+      <div>
+        <Button forest>
+          <TfiFaceSmile />
+          Satisfied?
+        </Button>
+      </div>
+      <div>
+        <Button alert>
+          <MdOutlineLogout />
+          Logout
+        </Button>
+      </div>
+      <div>
+        <Button blaze>
+          <TfiFaceSad />
+          Report Issue
+        </Button>
+      </div>
+      <div>
+        <Button inform>
+          <IoMdInformationCircleOutline />
+          Information
+        </Button>
+      </div>
+      <div>
+        <Button prime rounded>
+          <HiArrowLeft />
+          Prev
+        </Button>
+      </div>
+      <div>
+        <Button support rounded>
+          <HiArrowRight />
+          Next
+        </Button>
+      </div>
+      <div>
+        <Button prime outline>
+          <MdPowerSettingsNew />
+          Power
+        </Button>
+      </div>
+      <div>
+        <Button support outline>Buttons</Button>
+      </div>
+      <div>
+        <Button forest outline>Outline</Button>
+      </div>
+      <div>
+        <Button alert outline>Buttons</Button>
+      </div>
+      <div>
+        <Button blaze outline>
+          <AiOutlineHome />
+          Home
+        </Button>
+      </div>
+      <div>
+        <Button inform outline>Buttons</Button>
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ archive/version-2/tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        prime: '#ff4500',
+        support: '#008080',
+        light: '#fff',
+        dark: '#000',
+        forest: '#228b22',
+        alert: '#ffd700',
+        blaze: '#b41818',
+        inform: '#0f65cf'        
+      },
+    },
+  },
+  plugins: [],
+}
+
