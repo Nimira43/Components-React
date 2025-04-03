d34f8c1212562b249e9966fd70c5217e43dfcdab
Nimira43 <n.rabone72@aol.com>
2025-04-03 04:21:17 +0100

    Created planets array of objects.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,22 @@ import React from 'react'
 import Accordion from './components/Accordion'
 
 function App() { 
+  const planets = [
+    {
+      label: 'Jupiter',
+      content: '5th planet from the Sun'
+    },
+    {
+      label: 'Venus',
+      content: '2nd planet from the Sun'
+    },
+    {
+      label: 'Neptune',
+      content: '8th planet from the Sun'
+    }
+  ]
+
+
   return (
     <div>
       <Accordion />
