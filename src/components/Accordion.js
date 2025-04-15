ac12bc8c77e5b1eb0e18c0e23a951df49aa46df8
Nimira43 <n.rabone72@aol.com>
2025-04-15 18:53:50 +0100

    Created new isExpanded variable.

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -5,12 +5,9 @@ function Accordion({ items }) {
   const [expandedIndex, setExpandedIndex] = useState(0)
 
   const renderedItems = items.map((item, index) => {
-    if (index === expandedIndex) {
-      console.log('expanded')
-    } else {
-      console.log('collapsed')
-    }
-    
+    const isExpanded = index === expandedIndex
+
+
     return (
       <div key={item.id}>
         <div>{item.label}</div>
