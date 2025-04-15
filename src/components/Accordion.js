fcbc65368ca2a270f53da26abaa5e061d04dff66
Nimira43 <n.rabone72@aol.com>
2025-04-15 18:51:04 +0100

    Console logging expandedIndex state.

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -3,8 +3,14 @@ import { useState } from 'react'
 
 function Accordion({ items }) { 
   const [expandedIndex, setExpandedIndex] = useState(0)
-  
-  const renderedItems = items.map((item) => {
+
+  const renderedItems = items.map((item, index) => {
+    if (index === expandedIndex) {
+      console.log('expanded')
+    } else {
+      console.log('collapsed')
+    }
+    
     return (
       <div key={item.id}>
         <div>{item.label}</div>
