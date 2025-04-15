96df8178ef378f21d17af5fa4c5fac5715792b34
Nimira43 <n.rabone72@aol.com>
2025-04-15 20:11:22 +0100

    Shuffled the planets around in the items array.

--- src/constants/planets.js
+++ src/constants/planets.js
@@ -1,13 +1,13 @@
 export const items = [
   {
     id: '001',
-    label: 'Jupiter',
-    content: 'Jupiter is the fifth planet from the Sun and the largest in the Solar System. It is a gas giant with a mass more than 2.5 times that of all the other planets in the Solar System combined and slightly less than one-thousandth the mass of the Sun. Its diameter is eleven times that of Earth and a tenth that of the Sun.'
+    label: 'Pluto',
+    content: 'Pluto is the 9th planet in the Solar System. It is the ninth-largest and tenth-most-massive known object to directly orbit the Sun. It is the largest known trans-Neptunian object by volume by a small margin, but is less massive than Eris. Like other Kuiper belt objects, Pluto is made primarily of ice and rock and is much smaller than the inner planets. Pluto has roughly one-sixth the mass of the Moon, and one-third its volume.'
   },
   {
     id: '002',
-    label: 'Venus',
-    content: 'Venus is the second planet from the Sun. It is a terrestrial planet and is the closest in mass and size to its orbital neighbour Earth. Venus has by far the densest atmosphere of the terrestrial planets, composed mostly of carbon dioxide with a thick, global sulfuric acid cloud cover. '
+    label: 'Mars',
+    content: 'Mars is the fourth planet from the Sun. The surface of Mars is orange-red because it is covered in iron oxide dust, giving it the nickname "the Red Planet". Mars is among the brightest objects in Earth\'s sky, and its high-contrast albedo features have made it a common subject for telescope viewing. It is classified as a terrestrial planet and is the second smallest of the Solar System\'s planets.'
   },
   {
     id: '003',
@@ -16,18 +16,18 @@ export const items = [
   },
   {
     id: '004',
-    label: 'Mars',
-    content: 'Mars is the fourth planet from the Sun. The surface of Mars is orange-red because it is covered in iron oxide dust, giving it the nickname "the Red Planet". Mars is among the brightest objects in Earth\'s sky, and its high-contrast albedo features have made it a common subject for telescope viewing. It is classified as a terrestrial planet and is the second smallest of the Solar System\'s planets.'
+    label: 'Jupiter',
+    content: 'Jupiter is the fifth planet from the Sun and the largest in the Solar System. It is a gas giant with a mass more than 2.5 times that of all the other planets in the Solar System combined and slightly less than one-thousandth the mass of the Sun. Its diameter is eleven times that of Earth and a tenth that of the Sun.'
   },
   {
     id: '005',
-    label: 'Eris',
-    content: 'Eris is the most massive and second-largest known dwarf planet in the Solar System. It is a trans-Neptunian object in the scattered disk and has a high-eccentricity orbit. Eris was discovered in January 2005 by a Palomar Observatory based team and verified later that year. It was named in September 2006 after the Greco Roman goddess of strife and discord. Eris is the ninth-most massive known object orbiting the Sun and the sixteenth-most massive overall in the Solar System (counting moons).'
+    label: 'Venus',
+    content: 'Venus is the second planet from the Sun. It is a terrestrial planet and is the closest in mass and size to its orbital neighbour Earth. Venus has by far the densest atmosphere of the terrestrial planets, composed mostly of carbon dioxide with a thick, global sulfuric acid cloud cover. '
   },
   {
     id: '006',
-    label: 'Saturn',
-    content: 'Saturn is the sixth planet from the Sun and the second largest in the Solar System, after Jupiter. It is a gas giant, with an average radius of about nine times that of Earth. It has an eighth the average density of Earth, but is over 95 times more massive. Even though Saturn is almost as big as Jupiter, Saturn has less than a third its mass.'
+    label: 'Eris',
+    content: 'Eris is the most massive and second-largest known dwarf planet in the Solar System. It is a trans-Neptunian object in the scattered disk and has a high-eccentricity orbit. Eris was discovered in January 2005 by a Palomar Observatory based team and verified later that year. It was named in September 2006 after the Greco Roman goddess of strife and discord. Eris is the ninth-most massive known object orbiting the Sun and the sixteenth-most massive overall in the Solar System (counting moons).'    
   },
   {
     id: '007',
@@ -36,8 +36,8 @@ export const items = [
   },
   {
     id: '008',
-    label: 'Pluto',
-    content: 'Pluto is the 9th planet in the Solar System. It is the ninth-largest and tenth-most-massive known object to directly orbit the Sun. It is the largest known trans-Neptunian object by volume by a small margin, but is less massive than Eris. Like other Kuiper belt objects, Pluto is made primarily of ice and rock and is much smaller than the inner planets. Pluto has roughly one-sixth the mass of the Moon, and one-third its volume.'
+    label: 'Saturn',
+    content: 'Saturn is the sixth planet from the Sun and the second largest in the Solar System, after Jupiter. It is a gas giant, with an average radius of about nine times that of Earth. It has an eighth the average density of Earth, but is over 95 times more massive. Even though Saturn is almost as big as Jupiter, Saturn has less than a third its mass.'
   },
   {
     id: '009',
