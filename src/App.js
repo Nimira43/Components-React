4eff65285dde7d4f4e6ad56a20d8ee30ef24fc75
Nimira43 <n.rabone72@aol.com>
2025-04-15 18:26:57 +0100

    Passed planets into Accordion items prop

--- src/App.js
+++ src/App.js
@@ -2,13 +2,11 @@ import React from 'react'
 import { planets } from './constants/planets'
 import Accordion from './components/Accordion'
 
-console.log(planets)
-
 function App() { 
 
   return (
     <div>
-      <Accordion />
+      <Accordion items={planets}/>
     </div>
   )
 }
