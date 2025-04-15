dc1e66ed2769c5e53170ba345748d1394e036258
Nimira43 <n.rabone72@aol.com>
2025-04-15 20:14:58 +0100

    Updated items array.

--- src/constants/planets.js
+++ src/constants/planets.js
@@ -32,7 +32,7 @@ export const items = [
   {
     id: '007',
     label: 'Earth',
-    content: 'Earth is the third planet from the Sun and the only astronomical object known to harbor life. This is enabled by Earth being an ocean world, the only one in the Solar System sustaining liquid surface water. Almost all of Earth\'s water is contained in its global ocean, covering 70.8% of Earth\'s crust. The remaining 29.2% of Earth\'s crust is land, most of which is located in the form of continental landmasses within Earth\'s land hemisphere.'
+    content: 'Earth is the third planet from the Sun and the only astronomical object known to harbour life. This is enabled by Earth being an ocean world, the only one in the Solar System sustaining liquid surface water. Almost all of Earth\'s water is contained in its global ocean, covering 70.8% of Earth\'s crust. The remaining 29.2% of Earth\'s crust is land, most of which is located in the form of continental landmasses within Earth\'s land hemisphere.'
   },
   {
     id: '008',
