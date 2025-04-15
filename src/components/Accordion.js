3b048d5148a520874009d670db613308ce6aefa7
Nimira43 <n.rabone72@aol.com>
2025-04-15 19:20:12 +0100

    Separating the handleClick logic.

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -4,16 +4,16 @@ import { useState } from 'react'
 function Accordion({ items }) { 
   const [expandedIndex, setExpandedIndex] = useState(0)
 
+  const handleClick = (nextIndex) => {
+    setExpandedIndex(nextIndex)
+  }
+  
   const renderedItems = items.map((item, index) => {
     const isExpanded = index === expandedIndex
 
-    const handleClick = () => {
-      setExpandedIndex(index)
-    }
-
     return (
       <div key={item.id}>
-        <div onClick={handleClick}>
+        <div onClick={() => handleClick(index)}>
           {item.label}
         </div>
         {isExpanded && <div>{item.content}</div>}
