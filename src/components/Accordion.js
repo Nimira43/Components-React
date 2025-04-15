1ff4e2a3bdf5d90fbc00a295c5f08ebb13ca0a1c
Nimira43 <n.rabone72@aol.com>
2025-04-15 19:07:42 +0100

    Added onClick function on item.label. Once label is click the content will appear.

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -2,14 +2,18 @@ import React from 'react'
 import { useState } from 'react'
 
 function Accordion({ items }) { 
-  const [expandedIndex, setExpandedIndex] = useState(0)
+  const [expandedIndex, setExpandedIndex] = useState(5)
 
   const renderedItems = items.map((item, index) => {
     const isExpanded = index === expandedIndex
 
     return (
       <div key={item.id}>
-        <div>{item.label}</div>
+        <div 
+          onClick={() => setExpandedIndex(index)}
+        >
+          {item.label}
+        </div>
         {isExpanded && <div>{item.content}</div>}
       </div>
     )
