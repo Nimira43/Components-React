99f95f73e72bd6dc098bc780fb7bd58bded54efb
Nimira43 <n.rabone72@aol.com>
2025-04-15 19:13:43 +0100

    Refactor onClick handler

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -2,16 +2,18 @@ import React from 'react'
 import { useState } from 'react'
 
 function Accordion({ items }) { 
-  const [expandedIndex, setExpandedIndex] = useState(5)
+  const [expandedIndex, setExpandedIndex] = useState(0)
 
   const renderedItems = items.map((item, index) => {
     const isExpanded = index === expandedIndex
 
+    const handleClick = () => {
+      setExpandedIndex(index)
+    }
+
     return (
       <div key={item.id}>
-        <div 
-          onClick={() => setExpandedIndex(index)}
-        >
+        <div onClick={handleClick}>
           {item.label}
         </div>
         {isExpanded && <div>{item.content}</div>}
