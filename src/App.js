35ce04b8a89408d9d9359d19ab545337ec110d35
Nimira43 <n.rabone72@aol.com>
2025-05-08 21:17:34 +0100

    Creating selection state variable in App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from 'react'
 import Dropdown from './components/Dropdown'
+import { useState } from 'react'
 import { options } from './constants/colours'
 
 function App() { 
+  const [selection, setSelection] = useState(null)
+
 
   return (
       <Dropdown options={options}/>
