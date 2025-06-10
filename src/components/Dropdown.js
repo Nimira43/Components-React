e629291a8506597054c0b3200e6a0b5d7cf72e95
Nimira43 <n.rabone72@aol.com>
2025-06-10 10:29:48 +0100

    Added styling and icon.

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useState } from 'react'
+import { AiOutlineCaretDown } from 'react-icons/ai'
 
 function Dropdown({
   options,
@@ -17,6 +18,7 @@ function Dropdown({
   const renderedOptions = options.map((option) => {
     return (
       <div
+        className='hover:bg-[#eee] rounded cursor-pointer p-1'
         onClick={() => handleOptionClick(option)}
         key={option.value}
       >
@@ -25,14 +27,18 @@ function Dropdown({
     )
   })
   return (
-    <div>
+    <div className='w-48 relative'>
       <div
+        className='flex justify-between items-center cursor-pointer border rounded p-3 shadow bg-[#fffcfa] w-full'
         onClick={handleClick}
       >
         {value?.label || 'Select...'}
+        <AiOutlineCaretDown />
       </div>
       {isOpen &&
-        <div>
+        <div 
+          className='absolute top-full border rounded p-3 shadow bg-[#fffcfa] w-full'
+        >
           {renderedOptions}
         </div>
       }
