c8fbbda11f6a3fa4fab8870905f400431403eac4
Nimira43 <n.rabone72@aol.com>
2025-07-03 20:21:02 +0100

    Returning those class names plust the rest of any other styling

--- src/components/Panel.js
+++ src/components/Panel.js
@@ -2,7 +2,16 @@ import classNames from 'classnames'
 
 function Panel({ children, className, ...rest}) {
   const finalClassNames = classNames(
-    'border rounded p-3 shadow bg-[#fffcfa] w-full'
+    'border rounded p-3 shadow bg-[#fffcfa] w-full',
+    className
+  )
+  return (
+    <div 
+      {...rest}
+      className={finalClassNames}
+    >
+      {children}
+    </div>
   )
 }
 
