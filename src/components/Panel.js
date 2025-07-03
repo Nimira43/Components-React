faeccc2852778dc17b4a17e4975eaf47b9f01a95
Nimira43 <n.rabone72@aol.com>
2025-07-03 20:17:22 +0100

    Created finalClassNames with some styling for Panel.

--- src/components/Panel.js
+++ src/components/Panel.js
@@ -1,7 +1,9 @@
 import classNames from 'classnames'
 
 function Panel({ children, className, ...rest}) {
-
+  const finalClassNames = classNames(
+    'border rounded p-3 shadow bg-[#fffcfa] w-full'
+  )
 }
 
 export default Panel
