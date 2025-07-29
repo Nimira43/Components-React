435ec8ac9abefc4c886539192d68c64834a8fb9a
Nimira43 <n.rabone72@aol.com>
2025-07-29 07:59:34 +0100

    Created copy of current version of App,js and placed it inside Dropdown.js

---
+++ src/pages/DropdownPage.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import Dropdown from './components/Dropdown'
+import { useState } from 'react'
+import { options } from './constants/colours'
+
+function App() { 
+  const [selection, setSelection] = useState(null)
+
+  const handleSelect = (option) => {
+    setSelection(option)
+  }
+
+  return (
+    <div className='flex'>
+      <Dropdown
+        options={options}
+        value={selection}
+        onChange={handleSelect}
+      />
+      <Dropdown
+        options={options}
+        value={selection}
+        onChange={handleSelect}
+      />
+    </div>
+  )
+}
+
+export default App
