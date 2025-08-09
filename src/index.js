0b37da43c28f1e91c826839b805595c986337b93
Nimira43 <n.rabone72@aol.com>
2025-08-09 03:31:56 +0100

    Cleaned up index.js

--- src/index.js
+++ src/index.js
@@ -4,34 +4,11 @@ import './index.css'
 import App from './App'
 import { NavigationProvider } from './context/navigation'
 
-// import DropdownPage from './pages/DropdownPage'
-// import ButtonPage from './pages/ButtonPage'
-// import AccordianPage from './pages/AccordionPage'
-
 const el = document.getElementById('root')
 const root = ReactDOM.createRoot(el)
 
-
-// const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  // <React.StrictMode>
-    <NavigationProvider> 
-      <App />
-   </NavigationProvider> 
-    
-    // {/* <ButtonPage /> */}
-    // {/* <AccordianPage /> */}
-    // {/* <DropdownPage /> */}
-  
-//  </React.StrictMode>
+  <NavigationProvider> 
+    <App />
+  </NavigationProvider> 
 )
-
-/**
- * Notes 
- * 
- * To switch to previous components uncomment
- * the relevant import and component call.
- * Comment out the import and component call
- * not needed.
- * 
- */
