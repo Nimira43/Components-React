d450163ec5e37259a7803bce995bbf7272848b61
Nimira43 <n.rabone72@aol.com>
2025-09-14 14:42:36 +0100

    Route function - 2

--- src/components/Route.js
+++ src/components/Route.js
@@ -2,8 +2,8 @@ import React from 'react'
 import { useContext } from 'react'
 import NavigationContext from '../context/navigation'
 
-function Route() {
-
+function Route({ path, children }) {
+  const { currentPath } = useContext(NavigationContext)
 }
 
 export default Route
