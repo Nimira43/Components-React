040ba9b261139f28afe26ec59494f6c8ec959025
Nimira43 <n.rabone72@aol.com>
2025-12-01 23:27:54 +0000

    Modal - 2

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,8 +1,12 @@
 function Modal() {
   return (
     <div>
-      <div className='absolute inset-0 bg-grey-2 opacity-80'> </div>
-      <div></div>
+      <div className='absolute inset-0 bg-grey-2 opacity-80'> 
+
+      </div>
+      <div className='absolute inset-40 p-10 bg-light'>
+
+      </div>
     </div>
   )
 }
