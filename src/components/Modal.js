d25417752180e666d501710df52b83f4098afc82
Nimira43 <n.rabone72@aol.com>
2026-01-04 13:04:27 +0000

    Preventing scrolling when modal is open.

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,6 +1,15 @@
 import ReactDOM from 'react-dom'
+import { useEffect } from 'react'
 
 function Modal({ onClose, children, actionBar }) {
+  useEffect(() => {
+    document.body.classList.add('overflow-hidden')
+
+    return () => {
+      document.body.classList.remove('overflow-hidden')
+    }
+  }, [])
+
   return ReactDOM.createPortal(
     <div className='fixed inset-0 flex items-center justify-center z-50'>
       <div
